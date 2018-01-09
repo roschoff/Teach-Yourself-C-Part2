6befb7a73352ea16d6591f11eda1a5b8b7278eaa
Yordan Latinov <yordan.latinov@fdiba.tu-sofia.bg>
2018-01-09 13:31:11 +0200

    examples from chapter 5.3

---
+++ Chapter5.3 Example1/4by5array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int twod[4][5];
+	int i, j;
+
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 5; j++) {
+			twod[i][j] = i * j;
+		}
+	}
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 5; j++) {
+			printf("%d ", twod[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
