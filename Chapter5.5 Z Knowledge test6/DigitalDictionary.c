89b8dfa985b5236ba9f405e79933e5e77e34a438
Yordan Latinov <yordan.latinov@fdiba.tu-sofia.bg>
2018-01-31 15:15:21 +0200

    adding an updated version that uses labels

--- Chapter5.5 Z Knowledge test6/DigitalDictionary.c
+++ Chapter5.5 Z Knowledge test6/DigitalDictionary.c
@@ -15,28 +15,24 @@ int main(void)
 	int i = 0;
 	char input[20];
 
+	again:
 	printf("Enter an English word to translate: ");
 	gets(input);
-
-	while (strcmp(dict[i][0], "")) {
-		if (!strcmp(dict[i][0], input)) {
-			printf("Translation of word %s: %s\n", input, dict[i][1]);
-			break;
-		}
-		i++;
-	}
-
-	if (!strcmp(dict[i][0], "")) {
-		printf("Not in the dictionary! ");
-	}
-	/*for (i = 0; strcmp(dict[i][0]); i++) {
+	for (i; strcmp(dict[i][0]); i++) {
 		if (!strcmp(dict[i][0], input)) {
 			printf("Translation of %s: %s\n", input, dict[i][1]);
-			i--;
+			i = 0;
+			goto again;
 		}
-		printf("Enter an English word to translate: ");
-		gets(input);
-	}*/
+		else if (!strcmp(dict[i][0], "")) {
+			printf("Not in the dictionary! ");
+			i = 0;
+			goto again;
+		}
+		else if (!strcmp(input, "quit")) {
+			break;
+		}
+	}
 
 
 	return 0;
